6deb9dcf608cb02a40288b39278171337f4f2760
luis miguel <luismg9415@gmail.com>
2013-10-21 19:01:06 -0500

    listo parce proyecto creado!

---
+++ parcial2/src/main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author FHM
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        horarios jf = new horarios();
+        jf.setVisible(true);
+    }
+}
